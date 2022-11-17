df7b1b7fd84a3a320aa417b0129b6a3ec3943db1
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-17 18:50:58 -0300

    feat: tested endpoints

--- lib/src/views/home/home_view.dart
+++ lib/src/views/home/home_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:random_user_api_flutter/src/models/persons_model.dart';
+import 'package:random_user_api_flutter/src/repositories/repository/persons_repository.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: PersonsRepository().getAllPersons(1),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.results!.length,
+              itemBuilder: (context, index) => Text('${snapshot.data!.results![index].name!.first!} ${snapshot.data!.results![index].name!.last!}'),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
