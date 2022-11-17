3810b75db90db0363b7c886a7ca6828a9fd78f9e
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-17 17:32:49 -0300

    feat: created and implemented networking service

--- lib/src/core/services/networking_service.dart
+++ lib/src/core/services/networking_service.dart
@@ -1,1 +1,9 @@
-class Networking {}
+import 'package:http/http.dart' as http;
+
+class Networking {
+  String url;
+
+  Networking({required this.url});
+
+  Future<http.Response> get({String path = ''}) => http.get(Uri.parse(url + path));
+}
