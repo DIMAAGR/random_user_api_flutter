6fbb3e571ff469ec460fc03d6b7a950e0e8d23f1
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-17 17:22:18 -0300

    feat: added main_app.dart

---
+++ lib/src/core/main/main_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:random_user_api_flutter/src/core/theme/theme.dart';
+import 'package:random_user_api_flutter/src/views/home/home_view.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeView(),
+      theme: AppTheme.lightAppTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
